e91dafb3bd585810814b7045e81154c728a1e383
Andrzej Michalski <113372492+Michalski84@users.noreply.github.com>
2024-04-21 18:48:07 +0200

    hw03-p1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const contactsRouter = require('./routes/api/contacts');
-const { v4: uuidv4 } = require('uuid');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -10,7 +10,42 @@ app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', contactsRouter);
+mongoose.connect('mongodb+srv://Andrzej:Muchomor@cluster0.clqhoh7.mongodb.net/db-contacts', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('Connection error:', error.message);
+  process.exit(1);
+});
+
+db.once('open', () => {
+  console.log('Database connection successful');
+});
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Set name for contact'],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Contact = mongoose.model('Contact', contactSchema); 
+
+app.use('/api', contactsRouter(Contact)); 
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
